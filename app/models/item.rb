36fa3dc5c3725003392d2b98712bcede8d7cd560
koh26 <sol.van7110@gmail.com>
2020-11-23 15:23:54 +0900

    itemモデルのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,29 @@
 class Item < ApplicationRecord
   belongs_to : user
   has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
   belongs_to_active_hash :fee
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :day
+
+  with_options presence: true do
+    validates :image
+    validates :title
+    validates :about
+
+    validates :price, format: { with: /\A[0-9]+\z/ },inclusion: { in: 300..9999999 }
+
+  end
+
+  with_options numericality: { other_than: 1 }  do
+    validates :category_id
+    validates :status_id
+    validates :fee_id
+    validates :prefecture_id
+    validates :day_id
+  end
+  
 end
