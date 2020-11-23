9df234fa3830dfc7fa626bda31323cf5fe80aaa1
koh26 <sol.van7110@gmail.com>
2020-11-23 12:34:42 +0900

    配送料の負担アソシエーションまで

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,5 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
+  belongs_to_active_hash :fee
 end
