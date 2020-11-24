1bab875270fce957f5d3981d6cbaa36c7039745c
koh26 <sol.van7110@gmail.com>
2020-11-24 11:02:21 +0900

    ファクトリーボットの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,12 +3,12 @@ FactoryBot.define do
     title                 { 'テスト' }
     # image                 {'ActionDispatch::Http::UploadedFile:0x00007ff9b2bbd2e0 @tempfile=#<Tempfile:/var/folders/h3/9v6_xygx5yn97pg39wgf7hpw0000gn/T/RackMultipart20201123-6970-8kvshs.jpg'}
     about                 { 'テストの説明' }
-    category_id           { '2' }
-    status_id             { '2' }
-    fee_id                { '2' }
-    prefecture_id         { '2' }
-    day_id                { '2' }
-    price                 { '3000' }
+    category_id           { 2 }
+    status_id             { 2 }
+    fee_id                { 2 }
+    prefecture_id         { 2 }
+    day_id                { 2 }
+    price                 { 3000 }
     association :user
 
     after(:build) do |item|
