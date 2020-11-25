82828de60d5b345d740ccc66ea376923dc36f474
koh26 <sol.van7110@gmail.com>
2020-11-25 10:31:08 +0900

    出品者以外削除できないコードの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @item = Item.all.order('created_at DESC')
@@ -35,9 +35,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    if user_signed_in? && @item.user.id == current_user.id
+    @item.destroy
     redirect_to root_path
+    end
   end
 
   private
